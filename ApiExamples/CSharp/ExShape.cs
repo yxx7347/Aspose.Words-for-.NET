0753bcce0b273f88d04d117bd3715563e44af15f
vyacheslav.deryushev <falleretic@gmail.com>
2018-03-20 12:41:27 +0700

    Add last test

--- ApiExamples/CSharp/ExShape.cs
+++ ApiExamples/CSharp/ExShape.cs
@@ -756,5 +756,86 @@ namespace ApiExamples
 
             doc.Save(MyDir + @"\Artifacts\EmptyValuesInChartData.docx");
         }
+
+        [Test]
+        public void HiddenDefaultValue()
+        {
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            // Insert chart.
+            Shape shape = builder.InsertChart(ChartType.Column, 432, 252);
+            Chart chart = shape.Chart;
+
+            // Clear demo data.
+            chart.Series.Clear();
+
+            // Fill data.
+            chart.Series.Add("AW Series 1",
+                new string[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" },
+                new double[] { 1.2, 0.3, 2.1, 2.9, 4.2 });
+
+            MemoryStream stream = new MemoryStream();
+            doc.Save(stream, SaveFormat.Docx);
+
+            shape = (Shape)doc.GetChild(NodeType.Shape, 0, true);
+            chart = shape.Chart;
+
+            Assert.AreEqual(false, chart.AxisX.Hidden);
+            Assert.AreEqual(false, chart.AxisY.Hidden);
+        }
+
+        [Test]
+        public void HideChartAxis()
+        {
+            //ExStart
+            //ExFor:CharAxis.Hidden
+            //ExSummary: Show how hidden char axis.
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            // Insert chart.
+            Shape shape = builder.InsertChart(ChartType.Column, 432, 252);
+            Chart chart = shape.Chart;
+
+            // Clear demo data.
+            chart.Series.Clear();
+
+            // Fill data.
+            chart.Series.Add("AW Series 1",
+                new string[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" },
+                new double[] { 1.2, 0.3, 2.1, 2.9, 4.2 });
+
+            // Hide the Y axis.
+            chart.AxisY.Hidden = true;
+            //ExEnd
+
+            doc.Save(MyDir + "TestHiddenAxis.docx");
+        }
+
+        [Test]
+        public void HideAllChartAxis()
+        {
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            // Insert chart.
+            Shape shape = builder.InsertChart(ChartType.Column, 432, 252);
+            Chart chart = shape.Chart;
+
+            // Clear demo data.
+            chart.Series.Clear();
+
+            // Fill data.
+            chart.Series.Add("AW Series 1",
+                new string[] { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" },
+                new double[] { 1.2, 0.3, 2.1, 2.9, 4.2 });
+
+            // Hide the Y axis.
+            chart.AxisX.Hidden = true;
+            chart.AxisY.Hidden = true;
+
+            doc.Save(MyDir + "TestHiddenAxis.docx");
+        }
     }
 }
