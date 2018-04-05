28fc55d32f14ebdf53a0cfe7ebf15e8f76cead23
vyacheslav.deryushev <falleretic@gmail.com>
2018-04-06 01:17:15 +0700

    Rework reporting engine tests

---
+++ ApiExamples/NetStandart/ApiExamples.NetCore/TestData/TestClasses/ImageTestClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace ApiExamples.TestData.TestClasses
+{
+    public class ImageTestClass
+    {
+        public Image Image { get; set; }
+        public Stream ImageStream { get; set; }
+        public byte[] ImageBytes { get; set; }
+        public string ImageUri { get; set; }
+
+        public ImageTestClass(Image image, Stream imageStream, byte[] imageBytes, string imageUri)
+        {
+            this.Image = image;
+            this.ImageStream = imageStream;
+            this.ImageBytes = imageBytes;
+            this.ImageUri = imageUri;
+        }
+    }
+}
